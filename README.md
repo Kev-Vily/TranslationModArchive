2d4944d9490b6dc26e83f26f900b04c08b5315b8
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-10-20 10:13:17 +0700

    Archive

---
+++ README.md
@@ -0,0 +1,10 @@
+<div align=center>
+<h1>Terjemahan Indonesia</h1>
+<img src=https://github.com/user-attachments/assets/61f12b94-dce6-45e5-9321-f1dcd823e281>
+</div>
+
+<div align=center>
+<p>An Indonesian Translation Mod only for V7 build 146</p>
+<p>( Not supported in V8 Beta Release )</p>
+<p> [ March 20 / 2025 ]</p>
+</div>
---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const mindusindo = Vars.mods.locateMod("terjemahan-indonesia").meta;
+
+mindusindo.displayName = Core.bundle.get("mod.terjemahan-indonesia.name");
+mindusindo.author = Core.bundle.get("mod.terjemahan-indonesia.author");
+mindusindo.description = Core.bundle.get("mod.terjemahan-indonesia.description");
+mindusindo.subtitle = Core.bundle.get("mod.terjemahan-indonesia.subtitle");
+
+const asthosus = Vars.mods.locateMod("asthosus").meta;
+
+asthosus.displayName = Core.bundle.get("mod.asthosus.name");
+asthosus.author = Core.bundle.get("mod.asthosus.author");
+asthosus.description = Core.bundle.get("mod.asthosus.description");
+asthosus.subtitle = Core.bundle.get("mod.asthosus.subtitle");
+
+const exoprosopa = Vars.mods.locateMod("exoprosopa").meta;
+
+exoprosopa.displayName = Core.bundle.get("mod.exoprosopa.name");
+exoprosopa.author = Core.bundle.get("mod.exoprosopa.author");
+exoprosopa.description = Core.bundle.get("mod.exoprosopa.description");
+exoprosopa.subtitle = Core.bundle.get("mod.exoprosopa.subtitle");
+
+const axoxium = Vars.mods.locateMod("axoxium").meta;
+
+axoxium.displayName = Core.bundle.get("mod.axoxium.name");
+axoxium.author = Core.bundle.get("mod.axoxium.author");
+axoxium.description = Core.bundle.get("mod.axoxium.description");
+axoxium.subtitle = Core.bundle.get("mod.axoxium.subtitle");
+
+const xanthia = Vars.mods.locateMod("xanthia-3").meta;
+
+xanthia.displayName = Core.bundle.get("mod.xanthia.name");
+xanthia.author = Core.bundle.get("mod.xanthia.author");
+xanthia.description = Core.bundle.get("mod.xanthia.description");
+xanthia.subtitle = Core.bundle.get("mod.xanthia.subtitle");
